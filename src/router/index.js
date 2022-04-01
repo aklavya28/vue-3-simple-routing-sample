1e7b5e4c9a67ca3a4cd4ae96cb7acb2223604272
Sunil Bhardwaj <aklavya28@gmail.com>
2022-04-01 10:15:32 +0530

    A exersise application for vue 3 router appplication

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import Jobs from '../views/jobs/jobs.vue'
+import NotFound from '../views/NotFound.vue'
+import JobDetail from '../views/jobs/JobDetail.vue'
 
 const routes = [
   {
@@ -10,10 +14,30 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: AboutView
+  },
+  {
+    path: '/jobs',
+    name: 'jobs',
+    component: Jobs
+  },
+  {
+    path: '/jobDetail/:id',
+    name: 'jobDetail',
+    component: JobDetail,
+    props: true
+  },
+  // redirect
+  {
+    path:'/all-jobs',
+    redirect:'/jobs'
+  },
+  // 404 page
+  {
+    path:'/:catchAll(.*)',
+    name: 'NotFound',
+    component:NotFound
+ 
   }
 ]
 
---
+++ src/views/jobs/jobs.vue
@@ -0,0 +1,65 @@
+<template>
+    <h1>Listed Jobs </h1>
+   <ul>
+       <li v-for="job in jobs" :key="job.id">
+           <router-link :to="{name: 'jobDetail', params:{id: job.id}}">
+                {{ job.title }}
+           </router-link>
+       </li>
+   </ul>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            jobs: [
+                {
+                    id:1,
+                    title: "Web designer ui Required",
+                    jd: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo tempore voluptates consectetur eligendi eius vitae rem placeat laudantium, doloribus fugit voluptatum voluptatem facere veritatis nobis eos recusandae impedit. Asperiores, consequuntur.",
+                },
+                {
+                    id:2,
+                    title: "Web web designer Required",
+                    jd: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo tempore voluptates consectetur eligendi eius vitae rem placeat laudantium, doloribus fugit voluptatum voluptatem facere veritatis nobis eos recusandae impedit. Asperiores, consequuntur.",
+                },
+                {
+                    id:3,
+                    title: "Web vue Developer Required",
+                    jd: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo tempore voluptates consectetur eligendi eius vitae rem placeat laudantium, doloribus fugit voluptatum voluptatem facere veritatis nobis eos recusandae impedit. Asperiores, consequuntur.",
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+ul {
+    list-style: none;
+    margin: 0 auto;
+    max-width: 230px;
+    padding: 0;
+}
+
+ul li {
+    margin: 0 auto;
+    padding: 0;
+}
+
+ul li > a {
+    display: block;
+    padding: 10px 13px;
+    text-decoration: none;
+    background: #e7e7e7;
+    margin-bottom: 10px;
+    border-radius: 5px;
+    color: #444;
+    transition: all ease .5s;
+}
+
+ul li > a:hover {
+    background: #cddc39;
+}
+</style>
